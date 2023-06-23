d730e725366a4a87c1c7c4fc86f60c82ec51a055
Felipe dos santos rubino <feliperubinosan09@gmail.com>
2023-06-23 20:26:14 -0300

    Tela de login adicionada e botao verde alterado

--- src/screens/Login/index.tsx
+++ src/screens/Login/index.tsx
@@ -1,29 +1,50 @@
-import React from 'react';
-import { Text,  TouchableOpacity} from 'react-native';
-import { Container } from './style';
+import React, { useState } from "react";
+import { Text, TouchableOpacity, Image, View } from "react-native";
+import { Container, StyledPhoto, StyledText, StyledDiv } from "./style";
 import { NativeStackNavigationProp } from "@react-navigation/native-stack";
 import { StackParams } from "../../routes";
 import { useNavigation } from "@react-navigation/native";
+import Fundo from "../../components/Fundo";
+import InputModal from "../../components/InputModal";
+import BotaoVerde from "../../components/BotaoVerde";
+
 const Login = () => {
-  
-  const navigation = useNavigation < NativeStackNavigationProp<StackParams>> ();
-    const handleCadastro = () => {
-      return (
-        navigation.navigate('Cadastro')
-      )
-    }
-    return (
+  const navigation = useNavigation<NativeStackNavigationProp<StackParams>>();
+  const handleCadastro = () => {
+    return navigation.navigate("Cadastro");
+  };
+  const [name, setName] = useState("");
+  const [senha, setSenha] = useState("");
+
+  return (
+    <Fundo colors={["#BD6F29", "#ffffff"]} start={[1, 0]} end={[0, 1]}>
+      <Container>
+        <StyledPhoto source={require("../../../assets/img/logo_size.png")} />
         <Container>
-            <TouchableOpacity onPress={handleCadastro}>
-                 <Text>Cadastro</Text>
-            </TouchableOpacity>
+          <StyledDiv>
+            <StyledText> UserName </StyledText>
+          </StyledDiv>
+          <InputModal
+            value={name}
+            placeholder={"Entre com o username:"}
+            onChangeText={setName}
+            keyboardType="default"
+            inputWidth={170}
             
-            {/* <TouchableOpacity onPress={handleHome}>
-                 <Text >Home</Text> 
-            </TouchableOpacity>
-             */}
+          />
+          <StyledText> Senha </StyledText>
+          <InputModal
+            value={senha}
+            placeholder={"Entre com a senha:"}
+            onChangeText={setSenha}
+            keyboardType="default"
+            inputWidth={170}
+          />
         </Container>
-    )
-}
+        <BotaoVerde textoBotao="Entrar"/>
+      </Container>
+    </Fundo>
+  );
+};
 
 export default Login;
