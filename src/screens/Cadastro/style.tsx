5783ec6c4344ba6704fcf0b3e1d64ef867b43dbc
joncosta-3097 <jon.vieira.costa@gmail.com>
2023-06-24 23:42:13 -0300

    Tela Cadastro e seus Componentes

--- src/screens/Cadastro/style.tsx
+++ src/screens/Cadastro/style.tsx
@@ -2,7 +2,62 @@ import styled from "styled-components/native"
 
 export const Container = styled.View`
   flex: 1;
-  background-color: 'black';
-  align-items: center;
-  justify-content: center;
+  width: 100%;
+  align-self: center;
+  justify-content: space-evenly;
 `
+
+export const PhotoPerfil = styled.Image`
+  width: 125px;
+  height: 125px;
+  border-radius: 100px;
+  object-fit: cover;
+  margin-top: 5%;
+  align-self: center;
+`;
+
+export const ContainerCustom = styled.View`
+flex-direction:row;
+
+width: 100%;
+
+`
+
+export const Label = styled.Text`
+   font-Size : 20px;
+   align-self: flex-start;
+   
+`;
+
+export const ViewLabel = styled.View`
+  width: 90%;
+ 
+ 
+  align-self: center;
+`
+export const ViewLabelCustom = styled.View`
+  width: 45%;
+  margin-left: 5%;
+  justify-content: center;
+
+ 
+`
+
+
+
+export const LabelRow = styled(Label)`
+  
+  
+  
+`
+export const ViewBotao= styled.View`
+  border-radius: 10px;
+  margin: 20px;
+  background-color: blue;
+  align-self: center;
+  justify-content: flex-end;
+`
+
+
+
+
