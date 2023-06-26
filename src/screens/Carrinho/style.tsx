40d19e98d9bbcfcaa4e39eafee3279ea9eadae51
CaduuuS2 <carloseduardod346@gmail.com>
2023-06-26 03:31:17 -0300

    Atualização do carrinho com logica completa exceto useState produtos

--- src/screens/Carrinho/style.tsx
+++ src/screens/Carrinho/style.tsx
@@ -1,8 +1,50 @@
 import styled from "styled-components/native"
 
 export const Container = styled.View`
-  flex: 1;
-  background-color: #000000,
-  align-items: center,
-  justify-content: center
+flex: 1;
+align-items: center;
+justify-content: flex-start;
+self-align: center;
+margin-top: 10%;
+width: 100%;
 `
+// export const CarrinhoVazio = styled.View`
+// background-color: #ffffff;
+// `
+export const ValorFreteView = styled.View`
+align-items: flex-start;
+`
+export const ValorTotalView = styled.View`
+align-items: flex-start;
+`
+export const TextoFormaPagamentoView = styled.View`
+  flex-direction: column; 
+  justify-content: space-between;
+  width: 80%;
+
+`
+export const FormaDePagamentoView = styled.View`
+  flex-direction: row;
+  justifyContent: space-between;
+  alignItems: center;
+  margin: 15px;
+`
+export const BotaoVerdeView = styled.View`
+margin: 10px;
+`
+export const TextoPadrao = styled.Text`
+ font-size : 23px;
+ text-align: center;
+ align-items: center;
+`
+export const CepView = styled.View` 
+align-items: flex-start;`
+
+export const AvisoCarrinho = styled.View`
+align-items: center;
+width: 80%;
+margin: 15px;
+`
+export const BotaoTipoPagamento = styled.TouchableOpacity`
+margin: 10px;`
+
--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,11 +1,19 @@
 import React from 'react'
 import { Text } from 'react-native';
 import { Container } from './style';
+import BotaoVerde from '../../components/BotaoVerde/index';
+import { useNavigation } from "@react-navigation/native";
+import { NativeStackNavigationProp } from "@react-navigation/native-stack";
+import { StackParams } from "../../routes";
 
 const Home = () => {
+  const navigation = useNavigation<NativeStackNavigationProp<StackParams>>();
   return (
     <Container>
-      <Text>Home</Text>
+      <BotaoVerde
+            textoBotao="Entrar"
+            onPress={() => navigation.navigate("Carrinho")}
+          />
     </Container>
   )
 }
