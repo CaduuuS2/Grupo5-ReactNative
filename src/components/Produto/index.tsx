7dd96c4060087dd809797382bb8c20926a2e8d70
RafaelVieiraCamara <rafael.vieira.rvc@gmail.com>
2023-06-26 22:18:04 -0300

    Componente produto e seu estilo criado

--- src/components/Produto/index.tsx
+++ src/components/Produto/index.tsx
@@ -1,23 +1,63 @@
 import React from 'react';
-import { Text } from 'react-native';
-import { Container } from './style';
-
+import { ScrollView, Text, View} from 'react-native';
+import { Container, Textos, BlocoConteudos, BlocoImagem, Item, IconeImg,  BlocoBotoes, Botoes, BlocoTextos, ConteudoPrincipal } from './style'
+import { NativeStackNavigationProp } from "@react-navigation/native-stack";
+import { StackParams } from "../../routes";
+import { useNavigation } from "@react-navigation/native";
+import { SlideFromRightIOS } from '@react-navigation/stack/lib/typescript/src/TransitionConfigs/TransitionPresets';
+import { MaterialIcons, AntDesign  } from '@expo/vector-icons'; 
 
 const Produto = () => {
-    
-    const mock = {
-        nome: "Harry Potrer",
-        descricao: "Livro muito legal",
-        favorito: false,
-        preco: 198.99
+  
+  const navigation = useNavigation < NativeStackNavigationProp<StackParams>> ();
+    const handleCadastro = () => {
+      return (
+        navigation.navigate('Cadastro')
+      )
     }
-    
     return (
         <Container>
-            <Text>Produto</Text>
+        <ConteudoPrincipal>
+          <BlocoConteudos>
+            <BlocoImagem source={require('../../../assets/img/HarryPotter.png')}></BlocoImagem>
+              <BlocoTextos>
+                <Item>
+                <Textos>Nome: Harry Potter </Textos>
+                </Item>
+                
+                <Item>
+                <Textos>Valor: 49,90</Textos>
+                </Item>
+                
+                <BlocoBotoes>
+                  
+                  <Botoes onPress={handleCadastro}>
+               
+                  {/*<IconeImg source={require('./carrinho.png')}></IconeImg>*/}
+                  
+                  <Text>ADD</Text>
+                  <MaterialIcons name="add-shopping-cart" size={24} color="black" />
+                  
+                            
+                  </Botoes>
+
+
+                  <Botoes onPress={handleCadastro}>
+               
+                    <Text>Favorito</Text>
+                    <AntDesign name="heart" size={24} color="black" />
+                            
+                  </Botoes>
+                
+                
+                </BlocoBotoes>
+
+              </BlocoTextos>
+            </BlocoConteudos> 
+
+          </ConteudoPrincipal>
         </Container>
     )
 }
-
 
 export default Produto;
