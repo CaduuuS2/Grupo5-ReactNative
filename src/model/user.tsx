7cba402cde3d98988579fbd4de8fd729911ae600
Felipe dos santos rubino <feliperubinosan09@gmail.com>
2023-06-28 21:18:36 -0300

    Service produto

--- src/model/user.tsx
+++ src/model/user.tsx
@@ -4,3 +4,13 @@ export interface IUser{
     username : string;
     email : string;
 }
+export interface IProduto {
+    produtoId: number;
+    nome: string;
+    descricao: string;
+    quantidadeEstoque: number;
+    data: string;
+    valorUnitario: number;
+    url: string;
+
+}
